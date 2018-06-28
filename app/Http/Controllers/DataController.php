56e8723a5a4bca538148567a60c347ca6894d643
Maxime Golfier <maxgolfier@gmail.com>
2018-06-28 22:45:02 +0200

    message

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -77,27 +77,29 @@ class DataController extends Controller
                 $items = json_decode($result->getBody());
                 foreach ($items as $item) {
                     foreach ($item as $data) {
-                        $device = Device::where('name' , $data->device)->first();
-                        if ($device == null) {
-                            $device = Device::create(['name' => $data->device, 'users_id' => Auth::user()->id]);
-                        }
-                        if (isset($data->computedLocation)) {
-                            $location = [];
-                            $i = 0;
-                            $max = 1;
-                            foreach ($data->computedLocation as $myinfo) {
-                                $location[$i] = $myinfo;
-                                $i++;
-                                $max++;
+                        if(isset($data->device)) {
+                            $device = Device::where('name' , $data->device)->first();
+                            if ($device == null) {
+                                $device = Device::create(['name' => $data->device, 'users_id' => Auth::user()->id]);
                             }
-                            $nb_batch = $max%4;
-                            $current= 0;
-                            for($j=0; $j<$nb_batch; $j++) {
-                                $data_captor = Data::where('time' , $data->time)->first();
-                                if ($data_captor == null) {
-                                    $data_captor = Data::create(['lat' => $location[$current+0], 'lng' => $location[$current+1],'radius' => $location[$current+2], 'time' => $data->time, 'source' => $location[$current+3], 'device_id' => $device->id]);
+                            if (isset($data->computedLocation)) {
+                                $location = [];
+                                $i = 0;
+                                $max = 1;
+                                foreach ($data->computedLocation as $myinfo) {
+                                    $location[$i] = $myinfo;
+                                    $i++;
+                                    $max++;
                                 }
-                                $current += 4;
+                                $nb_batch = $max%4;
+                                $current= 0;
+                                for($j=0; $j<$nb_batch; $j++) {
+                                    $data_captor = Data::where('time' , $data->time)->first();
+                                    if ($data_captor == null) {
+                                        $data_captor = Data::create(['lat' => $location[$current+0], 'lng' => $location[$current+1],'radius' => $location[$current+2], 'time' => $data->time, 'source' => $location[$current+3], 'device_id' => $device->id]);
+                                    }
+                                    $current += 4;
+                                }
                             }
                         }
                     }
